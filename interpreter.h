c768ff9199aacbb5f8446ec549f5d4d868e608ad
SashirFirebat <mieszkomierunski@gmail.com>
2016-05-24 01:40:57 +0200

    Update interpreter.h

--- interpreter.h
+++ interpreter.h
@@ -13,6 +13,10 @@ class Interpreter
     private:
         float getFloat(char* com, int startPos);
         int getInt(char* com, int startPos);
+        void M300(char* com);
+        void G1(char* com);
+        void G4(char* com);
+        void G92(char* com);
 
 
 };
