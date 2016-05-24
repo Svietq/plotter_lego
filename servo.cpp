b7baef792400b37ee2a3233777aed9cd6eb429d6
Svietq <mswietku@gmail.com>
2016-05-24 02:29:03 +0200

    Update servo.cpp

--- servo.cpp
+++ servo.cpp
@@ -9,3 +9,18 @@ Servo::~Servo()
 {
     //dtor
 }
+
+void Servo::setPen1()
+{
+    hMot3.rotAbs(90);
+}
+
+void Servo::setPen2()
+{
+    hMot3.rotAbs(-90);
+}
+
+void Servo::setOff()
+{
+    hMot3.rotAbs(0);
+}
