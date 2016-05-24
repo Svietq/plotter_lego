31eb02a7c61af244e5176918f50a0b7d8e865834
Svietq <mswietku@gmail.com>
2016-05-24 15:36:33 +0200

    Create file.h

---
+++ file.h
@@ -0,0 +1,18 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class File
+{
+    string m_file;
+public:
+    File(string file);
+    vector<char*> read_file();
+};
+
+#endif // FILE_H
