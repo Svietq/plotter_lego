146c8b73186e9309d14827a637c5670cdfa433cf
Svietq <mswietku@gmail.com>
2016-05-24 14:51:26 +0200

    Update engine.cpp

--- engine.cpp
+++ engine.cpp
@@ -4,3 +4,20 @@ Engine::Engine()
 {
 
 }
+
+Engine::Engine(int id):m_id(id)
+{
+
+}
+
+void Engine::rotate(int steps)
+{
+    if(m_id==1)
+    {
+        hMot1.rotRel(steps);
+    }
+    else if(m_id==2)
+    {
+        hMot2.rotRel(steps);
+    }
+}
