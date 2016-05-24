5d375bad1c3336987af071852f43886eeda1fcf5
Svietq <mswietku@gmail.com>
2016-05-24 13:51:20 +0200

    Update engine.h

--- engine.h
+++ engine.h
@@ -5,12 +5,14 @@ class Engine
 {
     const double m_radius{5}; //of pulley
     const double m_spc{720}; //steps per circle
+    const double m_rps{3}; //radians per step
 
 public:
     Engine();
     virtual ~Engine();
     void setLength(float x);
     float getLength();
+    const double m_engine_const = m_radius * m_rps;
 protected:
 private:
     double m_spmm; // steps per milimeter
