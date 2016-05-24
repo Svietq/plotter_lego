d66d49b1b2458e886b3674398981cda563b105b4
SashirFirebat <mieszkomierunski@gmail.com>
2016-05-24 02:12:45 +0200

    Add files via upload

---
+++ Servo.cpp
@@ -0,0 +1,11 @@
+#include "Servo.h"
+
+Servo::Servo()
+{
+    //ctor
+}
+
+Servo::~Servo()
+{
+    //dtor
+}
---
+++ Servo.h
@@ -0,0 +1,17 @@
+#ifndef SERVO_H
+#define SERVO_H
+
+
+class Servo
+{
+    public:
+        Servo();
+        void setPen1();
+        void setPen2();
+        void setOff();
+        virtual ~Servo();
+    protected:
+    private:
+};
+
+#endif // SERVO_H
